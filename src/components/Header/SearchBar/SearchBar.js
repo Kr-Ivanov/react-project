0f2f83c2b2acd0fb7cedf4a6eb5dca3c82b5f85d
Kr-Ivanov <ivailov.kristian@gmail.com>
2021-04-09 07:06:11 +0300

    finishing searchbar

--- src/components/Header/SearchBar/SearchBar.js
+++ src/components/Header/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import SearchIcon from '@material-ui/icons/Search';
 import { useHistory } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStateValue } from '../../../StateProvider';
 
 
@@ -8,24 +8,18 @@ import { useStateValue } from '../../../StateProvider';
 const SearchBar = () => {
     const [searchQuery, setSearchQuery] = useState('');
 
-    //const [{ search }, dispatch] = useStateValue();
     const history = useHistory();
 
     const searchItems = (e) => {
-        history.push(`/search/${searchQuery}`)
+
+        history.replace(`/search/${searchQuery}`)
+        setSearchQuery('');
+
     }
-
-
-    //if (search != searchQuery) {
-    //    dispatch({
-    //        type: 'SEARCH_VALUE',
-    //        search: searchQuery
-    //    })
-    //}
 
     return (
         <div className="header__search">
-            <input className="header__searchInput" type="text" onChange={(e) => setSearchQuery(e.target.value)} />
+            <input className="header__searchInput" value={searchQuery} type="text" onChange={(e) => setSearchQuery(e.target.value)} />
             <SearchIcon className="header__searchIcon" onClick={searchItems} />
         </div>
     )
